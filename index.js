4ce9e4d69a87f34866006391d8510f3564d5ab8a
essbee23 <87711661+essbee23@users.noreply.github.com>
2022-08-05 21:10:02 -0400

    Add files via upload
    Generate button
    Background Image
    Fetch API

---
+++ index.js
@@ -0,0 +1,62 @@
+const get_meal_btn = document.getElementById('get_meal');
+const meal_container = document.getElementById('meal');
+
+
+// window.addEventListener("click", () => {
+//     document.getElementsByClassName("button-primary").innerHTML = "test"
+// }) 
+
+get_meal_btn.addEventListener("click", () => {
+    fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+        .then(res => res.json())
+        .then(res => { createMeal(res.meals[0]) });
+});
+
+
+const createMeal = (meal) => {
+    const ingredients = [];
+
+    for (let i = 1; i <= 20; i++) {
+        if (meal[`strIngredient${i}`]) {
+            ingredients.push(`${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`)
+        }    
+    }
+	
+    const newInnerHTML = `
+		<div class="recipe">
+
+            <div id="header">
+            <h2>${meal.strMeal}</h2>
+                <img src="${meal.strMealThumb}" alt="Meal Image">
+            </div>
+
+            <div id="ingredients">
+                <h5>Ingredients:</h5>
+                <ul>
+                    ${ingredients.map(ingredient => `<li>${ingredient}</li>`).join('')}
+                </ul>
+            </div>
+
+            <div id="instructions">
+                <p>${meal.strInstructions}</p>
+            </div>
+
+        </div>`;
+	
+    meal_container.innerHTML = newInnerHTML;
+};
+
+
+
+//maybe?
+{/* <div class="video">
+                <iframe width="420" height="315"
+                    src="https://www.youtube.com/embed/${meal.strYoutube.slice(-11)}">
+                </iframe>
+</div> */}
+
+//respond with a recipe card frame around ingredients and instructions, not picture
+//change .que to <h2>${meal.strMeal}</h2>
+// add print button
+// add accessibility features? review inherited 
+// SOCIAL PANEL JS
